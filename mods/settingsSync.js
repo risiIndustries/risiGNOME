932981f63dc01a7877ffebd845cd849c29cc7d2e
PizzaLovingNerd <pizzalovingnerd@teknik.io>
2022-08-09 21:53:43 -0700

    Update settingsSync.js

--- mods/settingsSync.js
+++ mods/settingsSync.js
@@ -1,4 +1,4 @@
-const { Gio } = imports.gi;
+const { Gio, GLib } = imports.gi;
 
 var settingsSync = class {
     constructor () {
@@ -13,6 +13,7 @@ var settingsSync = class {
 
     sync_theme (setting, key) {
         if (key == "color-scheme") {
+            // Matches Gtk theme to light and dark settings
             switch (setting.get_string(key)) {
                 default:
                 case "prefer-light":
@@ -21,6 +22,9 @@ var settingsSync = class {
                 case "prefer-dark": setting.set_string("gtk-theme", "adw-gtk3-dark");
                 break;
             }
+
+            // Resetting custom colors
+            GLib.spawn_command_line_async('adwcolor reset');
         }
     }
 };
